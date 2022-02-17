0d5dd507a39816b141fffc8c78a3e53cb7f1bd1b
codermother <ozgecoskun_3@hotmail.com>
2022-02-18 00:42:04 +0300

    Add sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app-body">
+        <Sidebar />
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar-recentItem">
+      <span className="sidebar-hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar-top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar-avatar" />
+        <h2>Özge Coşkun</h2>
+        <h4>ozgecoskun_3@hotmail.com</h4>
+      </div>
+
+      <div className="sidebar-stats">
+        <div className="sidebar-stat">
+          <p>Who viewed you</p>
+          <p className="sidebar-statNumber">2,300</p>
+        </div>
+        <div className="sidebar-stat">
+          <p>Views on post</p>
+          <p className="sidebar-statNumber">2,570</p>
+        </div>
+      </div>
+
+      <div className="sidebar-bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
